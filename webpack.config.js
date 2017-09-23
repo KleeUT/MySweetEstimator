c993ff79c52d0c6775a5c4eb9ff8323e9b477976
Kleeut <klee.ut@gmail.com>
2017-09-23 18:52:37 +1000

    :sparkles: Rebuilt using static html

--- webpack.config.js
+++ webpack.config.js
@@ -1,27 +1,51 @@
-var webpack = require('webpack');
- 
+const path = require('path');
+const webpack = require('webpack');
 module.exports = {
-  entry: './src/app.jsx',
-  output: { path: __dirname, filename: 'bundle.js' },
+  context: path.resolve(__dirname, './src'),
+  entry: { app: './js/index.js' },
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, './dist'),
+    publicPath: '/dist',                          // New
+  },
+  devServer: {
+    contentBase: path.resolve(__dirname, './'),  // New
+  },
   module: {
-    loaders: [
-        {
-            test: /.jsx?$/,
-            loader: 'babel-loader',
-            exclude: /node_modules/,
-            query: {
-                presets: ['es2015', 'react']
-            }
+    rules: [
+      { test: /\.css$/, use: ['style-loader', 'css-loader'] },
+      {
+        test: /\.woff2?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+        use: 'url-loader?limit=10000',
+      },
+      {
+        test: /\.(ttf|eot|svg)(\?[\s\S]+)?$/,
+        use: 'file-loader',
+      },
+      {
+        test: /\.(jpg|png|svg)$/,
+        loader: 'url-loader',
+        options: {
+          limit: 25000,
         },
-        { test: /\.css$/, loader: 'style-loader!css-loader' },
-        { test: /\.eot(\?v=\d+\.\d+\.\d+)?$/, loader: "file" },
-        { test: /\.(woff|woff2)$/, loader:"url?prefix=font/&limit=5000" },
-        { test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/, loader: "url?limit=10000&mimetype=application/octet-stream" },
-        { test: /\.svg(\?v=\d+\.\d+\.\d+)?$/, loader: "url?limit=10000&mimetype=image/svg+xml" }
+      },
+      {
+        test: /\.(eot|svg|ttf|woff|woff2)$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {}  
+          }
+        ]
+        // loader: 'file?name=public/fonts/[name].[ext]'
+    },
+      // Use one of these to serve jQuery for Bootstrap scripts:
+
+      // Bootstrap 4
+      { test: /bootstrap\/www\/js\/umd\//, use: 'imports-loader?jQuery=jquery' },
+
+      // Bootstrap 3
+      { test: /bootstrap-sass\/assets\/javascripts\//, use: 'imports-loader?jQuery=jquery' },
     ],
   },
-   externals:{
-     "$":"$",
-     "jQuery":"jQuery",
-   }
 };
